079aed83f4d532a5bd598f875ae131c1a683f018
gonzabt3 <fm_gonza@hotmail.com>
2017-07-03 20:01:51 -0300

    primer commit

---
+++ peleadores/Peleador.py
@@ -0,0 +1,14 @@
+import pygame
+
+class Peleador(pygame.sprite.Sprite):
+    def __init__(self,imagen,nombre,vida,golpe):
+        self.nombre=nombre
+        self.vida=vida
+        self.golpe=golpe
+        self.imagen=imagen
+        self.rect=self.imagen.get_rect()
+        self.rect.top,self.rect.left=(100,200)
+    def mover(self,vx,vy):
+       pass
+    def update(self,superficie):
+        superficie.blit(self.imagen,self.rect)
