93156277328877e576f863a51587160cefcffef5
gonzabt3 <fm_gonza@hotmail.com>
2017-07-06 20:30:42 -0300

    subZero bailando

--- imagenes/cody/cody.py
+++ imagenes/cody/cody.py
@@ -1,18 +1,19 @@
 import pygame
-from threading import Timer
-class Peleador(pygame.sprite.Sprite):
-    def __init__(self,imagenesQuieto):
 
-        self.imagenQuieto=imagenesQuieto
-        # self.imagen1=imagen1
-        # self.imagen2=imagen2
-        # self.imagen3=imagen3
-        # self.imagenGolpe=imagenGolpe
-        # self.imagenGolpe2 = imagenGolpe2
-        # self.imagenesPuno=[self.imagenGolpe,self.imagenGolpe2]
-        # self.imagenes = [self.imagen1, self.imagen2,self.imagen3]
+class Peleador(pygame.sprite.Sprite):
+    def __init__(self,imagen1,imagen2,imagen3,imagenGolpe,imagenGolpe2):
+        #self.nombre=nombre
+        #self.vida=vida
+        #self.golpe=golpe
+        self.imagen1=imagen1
+        self.imagen2=imagen2
+        self.imagen3=imagen3
+        self.imagenGolpe=imagenGolpe
+        self.imagenGolpe2 = imagenGolpe2
+        self.imagenesPuno=[self.imagenGolpe,self.imagenGolpe2]
+        self.imagenes = [self.imagen1, self.imagen2,self.imagen3]
         self.imagenActual=0
-        self.imagen=self.imagenQuieto[self.imagenActual]
+        self.imagen=self.imagenes[self.imagenActual]
         self.rect=self.imagen.get_rect()
         self.rect.top,self.rect.left=(100,200)
         self.move=False
@@ -20,35 +21,38 @@ class Peleador(pygame.sprite.Sprite):
     def mover(self, vx, vy): #metodo que mueve al chabon
         self.rect.move_ip(vx, vy)
 
-    def update(self, superficie,vx,vy):
+    def update(self, superficie,vx,vy,t,spacesigueapretada):
         if(vx==0 and vy==0):
             self.move=False #me fijo si se esta moviendo
-            self.estarQuieto(superficie)
         else:
             self.move=True
 
+        if(t>=0 and self.move==True):
+            self.nextimage()
 
 
+        self.mover(vx, vy)
+        self.imagen=self.imagenes[self.imagenActual]
 
 
-    # def pegarGolpe(self,spacesigueapretada,superficie):#pegar golpe
-    #
-    #     if(spacesigueapretada==True):
-    #         for i in range(2):
-    #             self.imagen = self.imagenesPuno[i]
-    #             superficie.blit(self.imagen,self.rect)
-    #             print i
-    #
-    def estarQuieto(self,superficie):
-            print self.imagenActual
-            self.nextImage(self.imagenActual,self.imagenQuieto)
-            superficie.blit(self.imagenQuieto[self.imagenActual], self.rect)
+        if not(self.pegarGolpe(spacesigueapretada,superficie)): #sino pega imprime al chabon caminando
+            superficie.blit(self.imagen, self.rect)
 
-    def nextImage(self,imagenActual,imagenesSec):#cambio de imagen:
+    def nextimage(self):#cambio de imagen:
         self.imagenActual += 1
 
-        if self.imagenActual > (len(imagenesSec) - 1):  # si se fue de rango que lo ponga en 0
+        if self.imagenActual > (len(self.imagenes) - 1):  # si se fue de rango que lo ponga en 0
             self.imagenActual = 0
+
+    def pegarGolpe(self,spacesigueapretada,superficie):#pegar golpe
+
+        if(spacesigueapretada==True):
+            for i in range(2):
+                self.imagen = self.imagenesPuno[i]
+                superficie.blit(self.imagen,self.rect)
+                print i
+        
+
 
 def main():
 
@@ -58,19 +62,12 @@ def main():
     salir = False
     reloj1 = pygame.time.Clock()
 
-    imagenParado1 = pygame.image.load("imagenes/zub_zero/parado/parado1.png").convert_alpha()
-    imagenParado2 = pygame.image.load("imagenes/zub_zero/parado/parado2.png").convert_alpha()
-    imagenParado3 = pygame.image.load("imagenes/zub_zero/parado/parado3.png").convert_alpha()
-    imagenParado4 = pygame.image.load("imagenes/zub_zero/parado/parado4.png").convert_alpha()
-    imagenParado5 = pygame.image.load("imagenes/zub_zero/parado/parado5.png").convert_alpha()
-    imagenParado6 = pygame.image.load("imagenes/zub_zero/parado/parado6.png").convert_alpha()
-    imagenParado7 = pygame.image.load("imagenes/zub_zero/parado/parado7.png").convert_alpha()
-    imagenParado8 = pygame.image.load("imagenes/zub_zero/parado/parado8.png").convert_alpha()
-    imagenParado9 = pygame.image.load("imagenes/zub_zero/parado/parado9.png").convert_alpha()
-    imagenParado10 = pygame.image.load("imagenes/zub_zero/parado/parado10.png").convert_alpha()
-    imagenParadoArray =[imagenParado1,imagenParado2,imagenParado3,imagenParado4,imagenParado5,imagenParado6,imagenParado7,imagenParado8,imagenParado9,imagenParado10]
-
-    player1 = Peleador(imagenParadoArray)
+    imagen1 = pygame.image.load("imagenes/cody/cody_pelea1.png").convert_alpha()
+    imagen2  = pygame.image.load("imagenes/cody/cody_pelea2.png").convert_alpha()
+    imagen3 = pygame.image.load("imagenes/cody/cody_pelea3.png").convert_alpha()
+    imagenGolpe = pygame.image.load("imagenes/cody/cody_puno.png").convert_alpha()
+    imagenGolpe2 = pygame.image.load("imagenes/cody/cody_puno2.png").convert_alpha()
+    player1 = Peleador(imagen1,imagen2,imagen3,imagenGolpe,imagenGolpe2)
     vx, vy = 0, 0
     velocidad = 7
     t = 0
@@ -125,7 +122,7 @@ def main():
                     spacesigueapretada=False
 
 
-        reloj1.tick(18)
+        reloj1.tick(25)
         # auxiliar de la animacion
         t += 1
         if t > 1:
@@ -134,12 +131,13 @@ def main():
 
 
         pantalla.fill((200, 200, 200))
-        player1.update(pantalla, vx, vy)
+        player1.update(pantalla, vx, vy, t,spacesigueapretada)
         pygame.display.update()
 
     pygame.quit()
 
 
 main()
+
 
 
