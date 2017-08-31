8b124d642946ceff3d16743ce54fb3882c1a29b1
gonzabt3 <fm_gonza@hotmail.com>
2017-08-31 18:22:18 -0300

    agrego defensa

--- main.py
+++ main.py
@@ -1,27 +1,31 @@
 import pygame
 from threading import Timer
 class Peleador(pygame.sprite.Sprite):
-    def __init__(self,imagenesQuieto,imagenesMovimiento,imagenesPunio1,imagenesPatada1):
+    def __init__(self,imagenesQuieto,imagenesMovimiento,imagenesPunio1,imagenesPatada1,imagenesDefensa1):
 
         self.imagenesMovimiento=imagenesMovimiento
         self.imagenesQuieto=imagenesQuieto
         self.imagenesPunio1=imagenesPunio1
         self.imagenesPatada1=imagenesPatada1
+        self.imagenesDefensa1=imagenesDefensa1
         self.imagenActual=0
         self.imagen=self.imagenesQuieto[self.imagenActual]
-        self.rect=self.imagen   .get_rect()
+        self.rect=self.imagen.get_rect()
         self.rect.top,self.rect.left=(100,200)
         self.move=False
-
-    def mover(self, vx, vy): #metodo que mueve al chabon
+        self.estado=0 # 0=inactivo,1=ataque,2=defensa,3=herido
+    def mover(self, vx, vy): #metodo que mueve al c habon
         self.rect.move_ip(vx, vy)
 
-    def update(self, superficie,vx,vy,fightMove,q_apretada,w_apretada):
+    def update(self, superficie,vx,vy,fightMove,q_apretada,w_apretada,defenseMove,e_apretada):
         if(fightMove==True): #PREGUNTO SI hay un movimiento de pelea
             if (q_apretada == True): #punio1
                 self.punio1(superficie)
             if (w_apretada == True): #patada1
                 self.patada2(superficie)
+        elif (defenseMove == True):
+            if (e_apretada==True):
+                self.defensa1(superficie)
         elif(vx==0 and vy==0): #si la velocida esta en 0 no se mueve
             self.move=False #me fijo si se esta moviendo
             self.estarQuieto(superficie)
@@ -30,7 +34,9 @@ class Peleador(pygame.sprite.Sprite):
                 self.mover(vx, vy)
                 self.moverse(superficie)
 
+
     def estarQuieto(self,superficie): #funcion que anima al personaj cuando esta quieto
+            self.estado=0;
             self.nextImage(self.imagenesQuieto)
             superficie.blit(self.imagenesQuieto[self.imagenActual], self.rect)
 
@@ -43,6 +49,7 @@ class Peleador(pygame.sprite.Sprite):
     #         superficie.blit(self.imagenesPunio1[self.imagenActual],self.rect)
 
     def punio1(self,superficie):
+        self.estado=1
         for i in range(0, 3):
             # print i
             if self.imagenActual>len(self.imagenesPunio1):
@@ -55,6 +62,7 @@ class Peleador(pygame.sprite.Sprite):
                 superficie.blit(self.imagenesPunio1[self.imagenActual],self.rect)
 
     def patada2(self,superficie):
+        self.estado=1
         for i in range(0, 3):
             # print i
             if self.imagenActual>len(self.imagenesPatada1):
@@ -66,17 +74,21 @@ class Peleador(pygame.sprite.Sprite):
             if self.imagenActual < len(self.imagenesPatada1):
                 superficie.blit(self.imagenesPatada1[self.imagenActual],self.rect)
         
+    def defensa1(self,superficie):
+        self.estado=2
+        self.nextImageLimitado(self.imagenesDefensa1);
+        if(self.imagenActual<=len(self.imagenesDefensa1)-1):
+            superficie.blit(self.imagenesDefensa1[self.imagenActual],self.rect)
+        else:
+            self.imagenActual=0
 
     def nextImageLimitado(self,imagenesSec):
 
         if(self.imagenActual<len(imagenesSec)-1):
             self.imagenActual+=1
 
-
-
     def nextImage(self,imagenesSec):#cambio de imagen:
         self.imagenActual += 1
-
         if self.imagenActual > (len(imagenesSec) - 1):  # si se fue de rango que lo ponga en 0
             self.imagenActual = 0
 
@@ -87,7 +99,6 @@ class Peleador(pygame.sprite.Sprite):
             self.imagenActual = 0
 
 def main():
-
 
     pygame.init()
     pantalla = pygame.display.set_mode((600, 600))
@@ -130,15 +141,21 @@ def main():
     imagenPatada2 = pygame.image.load("imagenes/zub_zero/patada/patada2.png").convert_alpha()
     imagenPatada3 = pygame.image.load("imagenes/zub_zero/patada/patada3.png").convert_alpha()
     imagenPatada4 = pygame.image.load("imagenes/zub_zero/patada/patada4.png").convert_alpha()
+    imagenPatadaArray = [imagenPatada1, imagenPatada2, imagenPatada3, imagenPatada4]
 
-    imagenPatadaArray=[imagenPatada1,imagenPatada2,imagenPatada3,imagenPatada4]
+    #imagenes chabon defensa1
+    imagenDefensa1 = pygame.image.load("imagenes/zub_zero/defensa/defensa1.png").convert_alpha()
+    imagenDefensa2 = pygame.image.load("imagenes/zub_zero/defensa/defensa2.png").convert_alpha()
+    imagenDefensa1Array =[imagenDefensa1,imagenDefensa2]
 
-    player1 = Peleador(imagenParadoArray,imagenMovimientoArray,imagenPunioArray,imagenPatadaArray)
+
+    player1 = Peleador(imagenParadoArray,imagenMovimientoArray,imagenPunioArray,imagenPatadaArray,imagenDefensa1Array)
     vx, vy = 0, 0
     velocidad = 7
     t = 0
-    leftsigueapretada, rightsigueapretada, upsigueapretada, downsigueapretada,q_apretada,w_apretada = False, False, False, False,False,False
+    leftsigueapretada, rightsigueapretada, upsigueapretada, downsigueapretada,q_apretada,w_apretada,e_apretada = False, False, False, False,False,False,False
     fightMove=False
+    defenseMove=False
     while salir != True:  # LOOP PRINCIPAL
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -162,6 +179,9 @@ def main():
                 if event.key == pygame.K_w:
                     w_apretada = True
                     fightMove=True
+                if event.key == pygame.K_e:
+                    e_apretada = True
+                    defenseMove=True
 
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT:
@@ -194,21 +214,21 @@ def main():
                 if event.key == pygame.K_w:
                     w_apretada=False
                     fightMove=False
+                if event.key == pygame.K_e:
+                    e_apretada=False
+                    defenseMove=False
 
         reloj1.tick(16)
-        # auxiliar de la animacion
-      #  t += 1
-       # if t > 1:
-        #    t = 0
-
-
+        #auxiliar de la animacion
+        #t += 1
+        #if t > 1:
+        #t = 0
 
         pantalla.fill((200, 200, 200))
-        player1.update(pantalla, vx, vy,fightMove,q_apretada,w_apretada)
+        player1.update(pantalla, vx, vy,fightMove,q_apretada,w_apretada,defenseMove,e_apretada)
         pygame.display.update()
 
     pygame.quit()
-
 
 main()
 
